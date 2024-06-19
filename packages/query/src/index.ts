f889f261c851e4dc5bc069ee8d168d193ff2aa42
saul <saul@organicdesign.co.nz>
2024-06-19 15:07:50 +0100

    Rework examples package to query package.

--- packages/query/src/index.ts
+++ packages/query/src/index.ts
@@ -7,27 +7,35 @@ import { EntityExtractor, ParagraphGenerator, Query } from '@organicdesign/gvs-l
 import { neo4jReader } from '@organicdesign/gvs-neo4j'
 import neo4j from 'neo4j-driver'
 import { collect, merge, take } from 'streaming-iterables'
+import assert from 'assert/strict'
 
-const QUERY = 'What is prompt prompt engineering?'
+assert(process.env.OLLAMA_TEMPERATURE)
+assert(process.env.OLLAMA_DIMENTIONS)
+assert(process.env.NEO4J_ADMIN_PASSWORD)
+
+if (process.env.QUERY == null) {
+  throw new Error('Missing QUERY environment variable.')
+}
 
 const model = new ChatOllama({
-  baseUrl: 'http://127.0.0.1:11434',
-  model: 'llama3:70b-instruct'
+  baseUrl: `http://${process.env.OLLAMA_HOST}:${process.env.OLLAMA_PORT}`,
+  model: process.env.OLLAMA_INFERENCE,
+  temperature: Number(process.env.OLLAMA_TEMPERATURE)
 })
 
 const embeddings = new OllamaEmbeddings({
-  baseUrl: 'http://127.0.0.1:11434',
-  model: 'snowflake-arctic-embed:latest'
+  baseUrl: `http://${process.env.OLLAMA_HOST}:${process.env.OLLAMA_PORT}`,
+  model: process.env.OLLAMA_EMBEDDINGS
 })
 
 // The normal embeddings vector store.
 const vectorStore = await Chroma.fromExistingCollection(
   embeddings,
   {
-    url: 'http://127.0.0.1:8000',
-    collectionName: 'embeddings',
-    collectionMetadata: { 'hnsw:space': 'cosine' },
-    numDimensions: 1024
+    url: `http://${process.env.CHROMA_HOST}:${process.env.CHROMA_PORT}`,
+    collectionName: process.env.CHROMA_EMBEDDING_COLLECTION,
+    collectionMetadata: { 'hnsw:space': process.env.CHROMA_SPACE },
+    numDimensions: Number(process.env.OLLAMA_DIMENTIONS)
   }
 )
 
@@ -35,14 +43,14 @@ const vectorStore = await Chroma.fromExistingCollection(
 const knowledgeGraphVectorStore = await Chroma.fromExistingCollection(
   embeddings,
   {
-    url: 'http://127.0.0.1:8000',
-    collectionName: 'knowledge-graph',
-    collectionMetadata: { 'hnsw:space': 'cosine' },
-    numDimensions: 1024
+    url: `http://${process.env.CHROMA_HOST}:${process.env.CHROMA_PORT}`,
+    collectionName: process.env.CHROMA_GRAPH_COLLECTION,
+    collectionMetadata: { 'hnsw:space': process.env.CHROMA_SPACE },
+    numDimensions: Number(process.env.OLLAMA_DIMENTIONS)
   }
 )
 
-const driver = neo4j.driver('neo4j://127.0.0.1', neo4j.auth.basic('neo4j', 'neo4j'))
+const driver = neo4j.driver(`neo4j://${process.env.NEO4J_HOST}`, neo4j.auth.basic('neo4j', process.env.NEO4J_ADMIN_PASSWORD))
 
 const entityExtractor = new EntityExtractor(model)
 const paragraphGenerator = new ParagraphGenerator(model)
@@ -55,7 +63,7 @@ const streamToIterator = async function * <T>(stream: IterableReadableStream<T[]
 }
 
 // Extract the entities from the query
-const stream = await entityExtractor.stream(QUERY)
+const stream = await entityExtractor.stream(process.env.QUERY)
 const itr = streamToIterator(stream)
 
 const extractLists = await collect(neo4jReader(driver, knowledgeGraphVectorStore, itr))
